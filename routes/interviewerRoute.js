a667bb27616a5da2c3fdce93f5a01de5a97927c0
Nilesh D <nileshlund@gmail.com>
2021-02-08 10:17:34 +0530

    Add routes to assign students to interviewers

--- routes/interviewerRoute.js
+++ routes/interviewerRoute.js
@@ -7,9 +7,11 @@ const {
   getInterviewer,
   updateInterviewer,
   deleteInterviewer,
+  assignStudentToInterviewer,
 } = require('../controllers/interviewerController');
 
 router.route('/').get(renderInterviewer).post(createInterviewer);
 router.route('/:id').get(getInterviewer).put(updateInterviewer).delete(deleteInterviewer);
+router.route('/:id/add_student').post(assignStudentToInterviewer);
 
 module.exports = router;
