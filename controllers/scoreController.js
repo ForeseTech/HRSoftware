c143fce97effb902d18e015a0a68800a910a1d51
Nilesh D <nileshlund@gmail.com>
2021-02-20 23:13:18 +0530

    Delete score button

--- controllers/scoreController.js
+++ controllers/scoreController.js
@@ -14,6 +14,20 @@ const renderScores = asyncHandler(async (req, res, next) => {
   res.render('score/index', { scores, name: req.user.name, role: req.user.role });
 });
 
+// @desc       Delete a score by ID
+// @route      GET /scores/:id
+// @access     Private (Admin)
+const deleteScore = asyncHandler(async (req, res, next) => {
+  // Find by Id and delete the user
+  const score = await Score.findById(req.params.id);
+  score.remove();
+
+  // Success Flash Message
+  req.flash('success', 'Score Successfully Deleted');
+  res.redirect('/scores');
+});
+
 module.exports = {
   renderScores,
+  deleteScore,
 };
--- routes/scoreRoute.js
+++ routes/scoreRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { renderScores } = require('../controllers/scoreController');
+const { renderScores, deleteScore } = require('../controllers/scoreController');
 
 const { isLoggedIn, authorize } = require('../middleware/auth');
 
 router.route('/').get(isLoggedIn, authorize('Admin'), renderScores);
+router.route('/:id').delete(isLoggedIn, authorize('Admin'), deleteScore);
 
 module.exports = router;
